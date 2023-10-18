fe0373952f4c68775a224cc9e9ee1cc90eb406e7
Sefa-Yilmaz <104129425+Sefa-Yilmaz@users.noreply.github.com>
2023-10-18 13:07:54 +0300

    flutter_music_player

---
+++ flutter_music_player/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_music_player/music_player_ui.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+       
+        primarySwatch: Colors.blue,
+      ),
+      home: const MusicPlayerUI()
+    );
+  }
+}
+
